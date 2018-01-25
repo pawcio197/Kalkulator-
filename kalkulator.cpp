b590680783149fad3ff68b3b69ee382e71af0250
AdamLipinski <adam.lipinskii@wp.pl>
2018-01-25 20:03:30 +0100

    wprowadzenie zmiennej string

--- kalkulator.cpp
+++ kalkulator.cpp
@@ -5,23 +5,24 @@
 #include "odejmowanie.h"
 #include "mnozenie.h"
 #include "dzielenie.h"
-
+#include <string>
 
 using namespace std;
 
 int main() 
 {
-int a,b,wybor;
+string wybor;
+int a,b;
 cout<<"Co chcesz zrobic?"<<endl.
 cout<<"1)Mnozenie"<<endl.
 cout<<"2)Dzielenie"<<endl.
 cout<<"3)Dodawanie"<<endl.
 cout<<"4)Odejmowanie"<<endl.
 cin >>wybor;
-if(wybor ==1){}
-else if (wybor ==2){}
-else if (wybor == 3){}
-else if (wybor ==4){}
+if(wybor = '1'){}
+else if (wybor ='2'){}
+else if (wybor ='3'){}
+else if (wybor ='4'){}
 else{}
 return 0;
 }
