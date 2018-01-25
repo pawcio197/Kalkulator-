71b3c1449dd1554dd083858433c160aaa6e0f040
johnsik1 <janek.wojtkowski@gmail.com>
2018-01-25 19:53:29 +0100

    dodanie funkcji wyboru

--- kalkulator.cpp
+++ kalkulator.cpp
@@ -11,9 +11,14 @@ using namespace std;
 
 int main() 
 {
-int a,b;
-cout<<"wprowadz a i b"
-cin >>a>>b;
+int a,b,wybor;
+cout<<"jakie opcje \n1.mnozenie\n2.dzielenie\n3.dodawanie\n4.odejmowanie"
+cin >>wybor;
+if(wybor ==1){}
+else if (wybor ==2){}
+else if (wybor == 3){}
+else if (wybor ==4){}
+else{}
 cout<<"zmienna a="<<a<<"zmienna b:"<<b<<endl;
 return 0;
 }
