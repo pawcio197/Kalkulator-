075b99cc978a3b9142f3afe8ae786409f7504dfb
Matusie214 <matusiakcezary@wp.pl>
2018-01-25 20:17:40 +0100

    dodano wywolanie funkcji

--- kalkulator.cpp
+++ kalkulator.cpp
@@ -19,10 +19,20 @@ cout<<"2)Dzielenie"<<endl.
 cout<<"3)Dodawanie"<<endl.
 cout<<"4)Odejmowanie"<<endl.
 cin >>wybor;
-if(wybor = '1'){}
-else if (wybor ='2'){}
-else if (wybor ='3'){}
-else if (wybor ='4'){}
-else{}
+if(wybor = '1'){
+mnozenie(a,b)
+}
+else if (wybor ='2'){
+dzielenie(a,b)
+}
+else if (wybor ='3'){
+dodawanie(a,b)
+}
+else if (wybor ='4'){
+odejmowanie(a,b)
+}
+else{
+cout << "Nie wybrales zadnej opcji"<<endl.
+}
 return 0;
 }
